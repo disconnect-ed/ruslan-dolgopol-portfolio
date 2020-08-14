bd9d13390f7dbd32c7bfc8e92d9b9eeb9880164c
disconnect-ed <ruslan199812@gmail.com>
2020-08-14 14:08:52 +0300

    Updates

---
+++ precache-manifest.5587eaeef3c14f394e15f78db10fe8e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142c2bbd08b19d3eba0655d00c2b570b",
+    "url": "/ruslan-dolgopol-portfolio/index.html"
+  },
+  {
+    "revision": "ba13f72d0ab23ec6ec4f",
+    "url": "/ruslan-dolgopol-portfolio/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "2db8c567d5af28c43356",
+    "url": "/ruslan-dolgopol-portfolio/static/css/main.73ff5a82.chunk.css"
+  },
+  {
+    "revision": "ba13f72d0ab23ec6ec4f",
+    "url": "/ruslan-dolgopol-portfolio/static/js/2.239dda0b.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/ruslan-dolgopol-portfolio/static/js/2.239dda0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db8c567d5af28c43356",
+    "url": "/ruslan-dolgopol-portfolio/static/js/main.da15e62f.chunk.js"
+  },
+  {
+    "revision": "abb2b144c06b9b40cc27",
+    "url": "/ruslan-dolgopol-portfolio/static/js/runtime-main.3aeee637.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/ruslan-dolgopol-portfolio/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/ruslan-dolgopol-portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/ruslan-dolgopol-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8af1ad7bc9bfdecc840355e2bad81742",
+    "url": "/ruslan-dolgopol-portfolio/static/media/avatar.8af1ad7b.jpg"
+  },
+  {
+    "revision": "44df6696421d42cbccd83fa71c3b790e",
+    "url": "/ruslan-dolgopol-portfolio/static/media/bg.44df6696.jpg"
+  },
+  {
+    "revision": "ef2d360f163af3ac74af6c737f38edf6",
+    "url": "/ruslan-dolgopol-portfolio/static/media/loading.ef2d360f.svg"
+  }
+]);
